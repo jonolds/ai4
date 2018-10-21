58da71bed8400e76d72eb12a81510b8c0eb9082b
Jon Olds <jonolds@gmail.com>
2018-10-20 23:58:57 -0500

    ai wins against self instead of tying. Added show all moves to view game moves across console

--- ai4final/TicTacToe.java
+++ ai4final/TicTacToe.java
@@ -3,30 +3,35 @@ import java.util.Scanner;
 
 public class TicTacToe {
 	Random r = new Random();
-	Scanner sc;
+	Scanner sc = new Scanner(System.in);
 	int moves_left = 9;
-	byte[] board;
+	byte[] board = new byte[] {1,2,3,4,5,6,7,8,9};
+	int[] move_history = new int[9];
+	boolean show_board = true;
 
 	void play() {
 		drawBoard();
 		humanMove();
-		for(int i = 0; i < 4; i++) {
+		while(moves_left > 0 && getWinner(board) == 0) {
 			aiMove();
-			drawBoard();
+			if(getWinner(board) != 0)
+				break;
 			humanMove();
 		}
 		results();
+		sc.close();
 	}
 	
 	void aiMove() {
-		drawBoard();
-		setO(board, minimax(copyBoard(board), moves_left, true, true));
+		int move = minimax(copyBoard(board), moves_left, true, true);
+		makeMove(board, move, false);
 	}
 	
 	int minimax(byte[] node, int depth, boolean maxTurn, boolean top) {
 		int best_move = -1;
-		if(depth == 0)
-			return getWinner(node);
+		int score = getWinner(node);
+		if(score != 0 || depth == 0)
+			return score;
 		if(maxTurn) {
 			int value = Integer.MIN_VALUE;
 			for(int i = 0; i < 9; i++) 	// each child of node do
@@ -67,27 +72,21 @@ public class TicTacToe {
 			char input = sc.next().charAt(0);
 			if(Character.isDigit(input))					//CHECK that char(0) is a digit
 				pos = Character.digit(input, 10);
-			if(!(pos > 0 && pos < 10))						//CHECK pos input range: 1 to 9
-				System.out.println("Move input must be an integer from 1 to 9.");
-			else if(board[pos-1] > 58)						//CHECK that pos-1 isn't taken
-				System.out.println("position " + (pos) + " already taken.");
-			else
-				break;
+			if(!(pos > 0 && pos < 10)) System.out.println("Move input must be an integer from 1 to 9.");
+			else if(board[pos-1] > 58)	System.out.println("position " + (pos) + " already taken.");
+			else break;
 			sc.reset();
 		}
-		setX(board, pos - 1);
+		makeMove(board, pos - 1, true);
 	}
 	
-	void setX(byte[] brd, int pos) { 
-		brd[pos] = 'X'; 
+	void makeMove(byte[] a_board, int pos, boolean isHuman) { 
+		a_board[pos] = (isHuman) ? (byte)'X' : (byte)'O';
+		move_history[9-moves_left] = pos;
 		moves_left--;
-	}
-	void setO(byte[] brd, int pos) { 
-		brd[pos] = 'O'; 
-		moves_left--;
+		drawBoard();
 	}
 	
-	byte[] copyBoard(byte[] bArr) { return bArr.clone(); }
 	int getWinner(byte[] s) {
 		byte[] win_rows = new byte[] {s[0],s[1],s[2],  s[3],s[4],s[5],  s[6],s[7],s[8],  s[0],s[3],s[6],
 									  s[1],s[4],s[7],  s[2],s[5],s[8],  s[0],s[4],s[8],  s[2],s[4],s[6]};
@@ -99,165 +98,107 @@ public class TicTacToe {
 	
 	void results() {
 		int winner = getWinner(board);
+		show_board = true;
 		System.out.println((winner == 0) ? "TIE GAME" : (winner == 1) ? "AI WINS" : "HUMAN WINS");
-		sc.close();
+		showAllMoves();
+		reset();
 	}
+	
+	public static void main(String[] args) {
+//		(new TicTacToe()).play();
+//		(new TicTacToe()).testRandomHuman(10000);
+		(new TicTacToe()).testOptimalHuman(1, false);
+	}
+
+	
+/* UTILITIES==================================== */
+	char ch(byte b) { return (b < 58) ? Byte.toString(b).charAt(0) : (char)b; }
+	
+	byte[] copyBoard(byte[] bArr) { return bArr.clone(); }
 	
 	void drawBoard() {
-		System.out.println();
-		System.out.println("*****MOVES LEFT=" + moves_left);
-		System.out.println(" " + ch(board[0]) + " | " + ch(board[1]) + " | " + ch(board[2]) + " ");
-		System.out.println("---+---+---");
-		System.out.println(" " + ch(board[3]) + " | " + ch(board[4]) + " | " + ch(board[5]) + " ");
-		System.out.println("---+---+---");
-		System.out.println(" " + ch(board[6]) + " | " + ch(board[7]) + " | " + ch(board[8]) + " ");
-	}
-	char ch(byte b) { return (b < 58) ? Byte.toString(b).charAt(0) : (char)b; }
-
-	TicTacToe() {
-		board = new byte[] {1,2,3,4,5,6,7,8,9};
-		sc = new Scanner(System.in);
-		moves_left = countOpenSpaces(board);
-	}
-	int countOpenSpaces(byte[] brd) {
-		int count = 0;
-		for(byte b: brd)
-			if(b < 59)
-				count++;
-		return count;
-	}
-	public static void main(String[] args) {
-		TicTacToe game = new TicTacToe();
-		game.play();
-		
-//		for(int i =0; i<10000; i++) {
-//			TicTacTest game = new TicTacTest();
-//			game.play();
-//		}
-	}
-}
-
-
-
-
-
-
-
-
-
-
-class TicTacTest {
-	Random r = new Random();
-	Scanner sc;
-	int moves_left = 9;
-	byte[] board;
-
-	TicTacTest() {
-		board = new byte[] {1,2,3,4,5,6,7,8,9};
-		
-		moves_left = countOpenSpaces(board);
+		if(show_board) {
+			System.out.println("\nLEFT=" + moves_left + "     ");
+			System.out.println(" " + ch(board[0]) + " | " + ch(board[1]) + " | " + ch(board[2]) + "\n---+---+---");
+			System.out.println(" " + ch(board[3]) + " | " + ch(board[4]) + " | " + ch(board[5]) + "\n---+---+---");
+			System.out.println(" " + ch(board[6]) + " | " + ch(board[7]) + " | " + ch(board[8]) + "\n---+---+---");
+		}
 	}
 	
-	void play() {
-		sc = new Scanner(System.in);
-		humanRandom();
-		for(int i = 0; i < 4; i++) {
-			aiMove();
-			humanRandom();
+	
+	void showAllMoves() {
+		int num = 9 - moves_left;
+		String[] str = new String[] {"", "", "", "", "", ""};
+		byte[] tmp_board = new byte[] {1,2,3,4,5,6,7,8,9};
+		boolean is_human = true;
+		for(int i = 0; i < num; i++) {
+			int space_num = move_history[i];
+			char piece = (is_human) ? 'X' : 'O';
+			tmp_board[space_num] = (byte)piece;
+			
+			str[0] += "LEFT=" + (8-i) + "       ";
+			str[1] += " " + ch(tmp_board[0]) + " | " + ch(tmp_board[1]) + " | " + ch(tmp_board[2]) + "   ";
+			str[2] += "---+---+---  ";
+			str[3] += " " + ch(tmp_board[3]) + " | " + ch(tmp_board[4]) + " | " + ch(tmp_board[5]) + "   ";
+			str[4] += "---+---+---  ";
+			str[5] += " " + ch(tmp_board[6]) + " | " + ch(tmp_board[7]) + " | " + ch(tmp_board[8]) + "   ";
+			is_human = !is_human;
 		}
-		results();
+		for(int i = 0; i < 6; i++)
+			System.out.println(str[i]);
+		
+	}
+	
+	void reset() {
+		moves_left = 9;
+		board = new byte[] {1,2,3,4,5,6,7,8,9};
+		move_history = new int[9];
+	}
+
+/* TESTING===============================================*/
+	
+	/* RANDOM HUMAN TESTING */
+	void testRandomHuman(int num_of_games, boolean show_moves) {
+		for(int i = 0; i < num_of_games; i++) {
+			show_board = show_moves;
+			randomHumanMove();
+			while(moves_left > 0 && getWinner(board) == 0) {
+				aiMove();
+				if(getWinner(board) != 0)
+					break;
+				randomHumanMove();
+			}
+			results();
+		}
 		sc.close();
 	}
 	
-	void aiMove() {
-		setO(board, minimax(copyBoard(board), moves_left, true, true));
-	}
-	
-	int minimax(byte[] node, int depth, boolean maxTurn, boolean top) {
-		int best_move = -1;
-		if(depth == 0)
-			return getWinner(node);
-		if(maxTurn) {
-			int value = Integer.MIN_VALUE;
-			for(int i = 0; i < 9; i++) 	// each child of node do
-				if(node[i] < 59) {
-					byte[] tmp = copyBoard(node);
-					tmp[i] = 'O';
-					int mm_value = minimax(tmp, depth-1, false, false);
-					if(mm_value > value) {
-						value = mm_value;
-						best_move = i;
-					}
-				}
-			if(top)
-				return best_move;
-			else
-				return value;
-		}
-		else {
-			int value = Integer.MAX_VALUE;
-			for(int i = 0; i < 9; i++)
-				if(node[i] < 59) {
-					byte[] tmp = copyBoard(node);
-					tmp[i] = 'X';
-					int mm_value = minimax(tmp, depth-1, true, false);
-					if(mm_value < value) {
-						value = mm_value;
-						best_move = i;
-					}
-				}
-			return value;
-		}
-	}
-
-	void humanRandom() {
+	void randomHumanMove() {
 		int pos = r.nextInt(9);
 		while(board[pos] > 58) {
 			pos = r.nextInt(9);
 		}
-		setX(board, pos);
+		makeMove(board, pos, true);
 	}
 	
-	void setX(byte[] brd, int pos) { 
-		brd[pos] = 'X'; 
-		moves_left--;
-	}
-	void setO(byte[] brd, int pos) { 
-		brd[pos] = 'O'; 
-		moves_left--;
-	}
-	
-	byte[] copyBoard(byte[] bArr) { return bArr.clone(); }
-	int getWinner(byte[] s) {
-		byte[] win_rows = new byte[] {s[0],s[1],s[2],  s[3],s[4],s[5],  s[6],s[7],s[8],  s[0],s[3],s[6],
-									  s[1],s[4],s[7],  s[2],s[5],s[8],  s[0],s[4],s[8],  s[2],s[4],s[6]};
-		for(int i = 0; i < 24; i+=3)
-			if(win_rows[i] > 59 && win_rows[i]==win_rows[i+1] && win_rows[i]==win_rows[i+2])
-				return (win_rows[i] == 'O') ? 1 : -1;
-		return 0;
+	/* OPTIMAL HUMAN TESTING  */
+	void testOptimalHuman(int num_of_games, boolean show_moves) {
+		for(int i = 0; i < num_of_games; i++) {
+			show_board = show_moves;
+			optimalHumanMove();
+			while(moves_left > 0 && getWinner(board) == 0) {
+				aiMove();
+				if(getWinner(board) != 0)
+					break;
+				optimalHumanMove();
+			}
+			results();
+		}
+		sc.close();
 	}
 	
-	void results() {
-		int winner = getWinner(board);
-		System.out.println((winner == 0) ? "TIE GAME" : (winner == 1) ? "AI WINS" : "HUMAN WINS");
-	}
-	
-	void drawBoard() {
-		System.out.println();
-		System.out.println("*****MOVES LEFT=" + moves_left);
-		System.out.println(" " + ch(board[0]) + " | " + ch(board[1]) + " | " + ch(board[2]) + " ");
-		System.out.println("---+---+---");
-		System.out.println(" " + ch(board[3]) + " | " + ch(board[4]) + " | " + ch(board[5]) + " ");
-		System.out.println("---+---+---");
-		System.out.println(" " + ch(board[6]) + " | " + ch(board[7]) + " | " + ch(board[8]) + " ");
-	}
-	char ch(byte b) { return (b < 58) ? Byte.toString(b).charAt(0) : (char)b; }
-
-	int countOpenSpaces(byte[] brd) {
-		int count = 0;
-		for(byte b: brd)
-			if(b < 59)
-				count++;
-		return count;
+	void optimalHumanMove() {
+		int move = minimax(copyBoard(board), moves_left, true, true);
+		makeMove(board, move, true);
 	}
 }
